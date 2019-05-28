f929e47e3e14b0c1f1f92f29b225d97311c5baf1
lbarazza <leonardo.barazza@gmail.com>
2019-05-28 10:52:08 -0600

    Solved some RunTime errors
    solved errors in the preprocess_experience function

--- DQNAgent.py
+++ DQNAgent.py
@@ -2,6 +2,7 @@ from collections import deque
 import random
 import numpy as np
 import torch
+import torch.nn.functional as F
 from models.QNet import QNet
 
 class Agent:
@@ -33,9 +34,13 @@ class Agent:
 
     # choose action based on epsilon-greedy policy if greedy=False, otherwise follow greedy policy
     def choose_action(self, state, greedy=False):
-        action = torch.argmax(self.qnet(Agent.preprocess_state(state)), dim=1).item()
-        p = 1-self.epsilon if not greedy else 1
-        return np.random.choice([action, np.random.randint(self.nA)], p=[p, 1-p])
+        if (random.random() > self.epsilon) or greedy:
+            action = torch.argmax(self.qnet(Agent.preprocess_state(state)), dim=1).item()
+        else:
+            action = np.random.randint(self.nA)
+        return action
+        #p = 1-self.epsilon if not greedy else 1
+        #return np.random.choice([action, np.random.randint(self.nA)], p=[p, 1-p])
 
     #
     def step(self, state, action, reward, new_state, done):
@@ -47,13 +52,11 @@ class Agent:
             self.experience_replay()
 
         #check whether to update target_qnet
-        if not (self.steps == self.target_qnet_update_rate):
-            self.steps += 1
-        else:
-            self.steps = 0
+        if self.steps % self.target_qnet_update_rate == 0:
             #update target_qnet parameters with qnet parameters
             self.target_qnet = self.qnet
 
+        self.steps += 1
         # decrease epsilon
         self.epsilon = max(self.epsilon_end, self.epsilon-self.epsilon_decay_rate)
 
@@ -76,28 +79,20 @@ class Agent:
     # extract all states, actions, rewards and new states from tuple and return a separate tensor for each
     @staticmethod
     def preprocess_experiences(experiences):
-        #print(experiences)
-        #torch.tensor(list(np.array(a)[:,0]))
-
-        experiences = np.array(experiences)
-        states = torch.tensor(list(experiences[:,0]))
-        actions = torch.tensor(list(experiences[:,1]))
-        rewards = torch.tensor(list(experiences[:,2]))
-        new_states = torch.tensor(list(experiences[:,3]))
-        dones = torch.tensor(list(experiences[:,4]))
-
-        #experiences = torch.tensor(experiences)
-        #states = experiences[:,0].view(-1, 1)
-        #actions = experiences[:, 1].view(-1, 1)
-        #rewards = experiences[:, 2].view(-1, 1)
-        #new_states = experiences[:, 3].view(-1, 1)
-        #dones = experiences[:, 4].view(-1, 1)
+        states = torch.tensor(np.vstack([i[0] for i in experiences])).float()
+        # we are going to use the actions variable as index to the gather function in learn(),
+        # by default PyTorch uses the Long datatype to refer to indeces to allow inexing of very large
+        # datasets (the int datatype would only allow for elements up to 4GB)
+        actions = torch.tensor(np.vstack([i[1] for i in experiences])).long()
+        rewards = torch.tensor(np.vstack([i[2] for i in experiences])).float()
+        new_states = torch.tensor(np.vstack([i[3] for i in experiences])).float()
+        dones = torch.tensor(np.vstack([i[4] for i in experiences]).astype(np.uint8)).float()
         return states, actions, rewards, new_states, dones
 
     # train the agent over a batch of experiences
     def learn(self, states, actions, rewards, new_states, dones):
         self.optimizer.zero_grad()
-        td_targets = rewards + self.gamma*(torch.argmax(self.target_qnet(new_states), dim=1)) * (1-dones)
+        td_targets = rewards + self.gamma*(torch.argmax(self.target_qnet(new_states), dim=1, keepdim=True)).float() * (1-dones)
         y = torch.gather(self.qnet(states), dim=1, index=actions)
         loss = F.mse_loss(y, td_targets)
         loss.backward()
