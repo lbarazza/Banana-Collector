1681f9bda3e1e198abe913deaf65ed4c6c87e1ee
lbarazza <leonardo.barazza@gmail.com>
2019-05-30 11:23:36 -0600

    Reduce number of hidden layers in QNet

--- QNet.py
+++ QNet.py
@@ -6,18 +6,18 @@ class QNet(nn.Module):
     def __init__(self, nS, nA): #nS: state space, nA: action space
         super(QNet, self).__init__()
 
-        self.input_layer = nn.Linear(nS, 200)
-        self.h_layer1 = nn.Linear(200, 200)
-        self.h_layer2 = nn.Linear(200, 200)
-        self.h_layer3 = nn.Linear(200, 200)
-        self.output_layer = nn.Linear(200, nA)
+        self.input_layer = nn.Linear(nS, 64)
+        self.h_layer1 = nn.Linear(64, 64)
+        self.h_layer2 = nn.Linear(64, 64)
+        self.h_layer3 = nn.Linear(64, 64)
+        self.output_layer = nn.Linear(64, nA)
 
     def forward(self, x):
 
         x = F.relu(self.input_layer(x))
         x = F.relu(self.h_layer1(x))
-        x = F.relu(self.h_layer2(x))
-        x = F.relu(self.h_layer3(x))
+        #x = F.relu(self.h_layer2(x))
+        #x = F.relu(self.h_layer3(x))
         x = self.output_layer(x)
 
         return x
