874775a0a6383a51b95afd4d828691846f15b214
lbarazza <leonardo.barazza@gmail.com>
2019-05-31 14:51:10 -0600

    Introduced dropout layers

--- QNet.py
+++ QNet.py
@@ -7,17 +7,16 @@ class QNet(nn.Module):
         super(QNet, self).__init__()
 
         self.input_layer = nn.Linear(nS, 64)
+        self.dropout = nn.Dropout(p=0.5)
         self.h_layer1 = nn.Linear(64, 64)
-        self.h_layer2 = nn.Linear(64, 64)
-        self.h_layer3 = nn.Linear(64, 64)
         self.output_layer = nn.Linear(64, nA)
 
     def forward(self, x):
 
         x = F.relu(self.input_layer(x))
+        x = self.dropout(x)
         x = F.relu(self.h_layer1(x))
-        #x = F.relu(self.h_layer2(x))
-        #x = F.relu(self.h_layer3(x))
+        x = self.dropout(x)
         x = self.output_layer(x)
 
         return x
